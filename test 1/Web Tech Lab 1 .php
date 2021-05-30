44f78f21856e1ffb526a4314ca613effcc177872
rukaiya <rukaiyadhara00@gmail.com>
2021-05-30 12:12:07 -0500

    lab task 1

---
+++ test 1/Web Tech Lab 1 .php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title> DHARA </title>
+</head>
+<body>
+<h1> MY HOME PAGE </h1>
+<marquee> <h2> My PROFILE </h2> </marquee>
+<a href="https://www.w3schools.com"> Visit W3schools </a>
+
+<form>
+First Name:<input type="text" name="fname"><br>
+Last Name:<input type="text"><br>
+Choose one options <br>
+Male<input type="radio" name="gender">
+Female<input type="radio" name="gender">
+<br>
+Music<input type="radio" name="hobby">
+Dance<input type="radio" name="hobby">
+<br>
+
+
+<input type="date">
+
+<input type="submit" value="submit">
+<input type="reset">
+
+</form>
+</body>
+</html>
